67ad26729f9063ec7a9969e2a73d8ca3d895b328
jedgodsey <jed.godsey@gmail.com>
2020-10-01 16:36:32 -0700

    show pages working

--- controllers/carCtrl.js
+++ controllers/carCtrl.js
@@ -1,6 +1,18 @@
 const express = require('express');  //read up on Router();
 const router = express.Router();
+const cars = require('../models/cars');
 
-// router.get('./')
+router.get('/', (req, res) => {
+    res.render('carIndex', {cars: cars})
+})
+
+router.get('/:index', (req, res) => {
+    let car = req.params.index;
+    res.render('show', {
+        cars: cars,
+        car: car
+    })
+})
+router.get
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = 5000;
 
-app.set('view-engine', 'ejs');
+app.set('view engine', 'ejs');
 
 const carCtrl = require('./controllers/carCtrl.js')
 
-app.use('/views', carCtrl); // how does this work?
+app.use('/cars', carCtrl); // how does this work?
 
-// app.use('/fruits', fruitsCtrl);
-
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', (req, res) => { //home route
+    res.render('../home')
 });
-
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
