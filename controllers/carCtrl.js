1243f26867f1b57560c7cca21dc9d3c64622ef4b
jedgodsey <jed.godsey@gmail.com>
2020-10-02 11:15:20 -0700

    delete button on show page not hitting path in controller

--- controllers/carCtrl.js
+++ controllers/carCtrl.js
@@ -2,17 +2,29 @@ const express = require('express');  //read up on Router();
 const router = express.Router();
 const cars = require('../models/cars');
 
+// all routes follow /cars
 router.get('/', (req, res) => {
     res.render('carIndex', {cars: cars})
 })
 
-router.get('/:index', (req, res) => {
-    let car = req.params.index;
+router.get('/:car', (req, res) => {
+    let car = req.params.car
     res.render('show', {
         cars: cars,
         car: car
     })
 })
-router.get
+
+router.delete('/:car', (req, res) => {
+    console.log('hitting the path')
+    cars.splice(req.params.car, 1);
+    res.redirect('/cars');
+})
 
 module.exports = router;
+
+
+// router.delete('/:fruitIndex', (req, res) => {
+//     fruits.splice(req.params.fruitIndex, 1);
+//     res.redirect('/fruits')
+//   })
