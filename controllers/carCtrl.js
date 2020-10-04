1e8c678f84447db4ffe8b0543310a98a42cce352
jedgodsey <jed.godsey@gmail.com>
2020-10-03 21:38:05 -0700

    redirect not showing changes

--- controllers/carCtrl.js
+++ controllers/carCtrl.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const cars = require('../models/cars');
 
 // all routes follow /cars
+
+// index of cars
 router.get('/', (req, res) => {
     res.render('carIndex', {cars: cars})
 })
 
+// show selected car
 router.get('/:car', (req, res) => {
     let car = req.params.car
     res.render('show', {
@@ -15,11 +18,13 @@ router.get('/:car', (req, res) => {
     })
 })
 
+// delete car
 router.delete('/:car', (req, res) => {
     cars.splice(req.params.car, 1);
     res.redirect('/cars');
 })
 
+// edit car
 router.get('/:car/edit', (req, res) => {
     let carIndex = req.params.car
     res.render('edit', {
@@ -29,7 +34,31 @@ router.get('/:car/edit', (req, res) => {
 })
 
 router.put('/:car', (req, res) => {
-    console.log('working maybe?')
+    let car = req.params.car;
+    console.log('fruit style ' + JSON.stringify(req.body));
+    let newCar = {
+        make: cars[car].make,
+        model: cars[car].model,
+        doors: cars[car].doors,
+        AWD: cars[car].AWD
+    }
+    cars.splice(car, 1, newCar);
+    res.redirect(`${car}`);
 })
+
+// router.put('/:car', (req, res) => {
+//     let car = req.params.car;
+//     console.log(req.body.name)
+//     const newCar = {
+//       make: req.body.make,
+//       model: req.body.model,
+//       doors: req.body.doors,
+//       AWD: req.body.AWD === 'on'
+//     }
+//     cars.splice(car, 1, newCar);
+//     res.redirect(`/cars/${car}`)
+//   })
+
+
 
 module.exports = router;
